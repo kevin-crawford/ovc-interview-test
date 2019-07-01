07989e7f7a2f26e85bb91a1b04fa15e8ca2db868
Kevin Crawford <kcdouglas92@gmail.com>
2019-07-01 14:59:53 -0700

    added redux store, created table aand cell components, added getdata action to hit api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './components/Table';
+
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
 
 function App() {
+  // headings are static, array of headings are provided to the component through headings prop
+  const headings = [
+    'Name',
+    'Email',
+    'City',
+    'Company'
+  ]
+  // provider gives us our redux store, data and UI
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Table headings={headings}/>
+    </Provider>
   );
 }
 
